d4a1487f69cae9289477b128359136bc7d3fe24d
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-08-09 17:14:59 +0100

    setup

---
+++ src/main/java/com/dalal/boukingandreviewservicepfe/BoukingAndReviewServicePfeApplication.java
@@ -0,0 +1,13 @@
+package com.dalal.boukingandreviewservicepfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoukingAndReviewServicePfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoukingAndReviewServicePfeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dalal/boukingandreviewservicepfe/BoukingAndReviewServicePfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalal.boukingandreviewservicepfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoukingAndReviewServicePfeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
