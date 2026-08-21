4c227212e0d5592bc2a1fc874bab36c3ceb4617e
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-08-21 11:23:58 +0100

    feat(event): add ReservationCreatedEvent

---
+++ src/main/java/com/dalal/boukingandreviewservicepfe/dtos/event/ReservationCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.dalal.boukingandreviewservicepfe.dtos.event;
+
+import com.dalal.boukingandreviewservicepfe.enums.BookingStatus;
+
+import java.time.LocalDateTime;
+
+public record ReservationCreatedEvent(
+        Long bookingId,
+        Long clientId,
+        Long providerId,
+        BookingStatus status,
+        LocalDateTime bookingDate,
+        LocalDateTime createdAt
+) {}
